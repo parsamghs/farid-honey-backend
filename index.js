58fb01c7417b54e84c00c408cd338732dcda0caa
Parsa <parsapy2005@gmail.com>
2025-08-19 09:18:12 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const pool = require('./Config/db');
+
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
