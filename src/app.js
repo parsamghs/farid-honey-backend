d7c0675d41c70c4c3be67d765275bfa9cb87c752
Parsa <parsapy2005@gmail.com>
2025-10-20 18:34:04 +0330

    fix cors

--- src/app.js
+++ src/app.js
@@ -18,14 +18,14 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     } else {
+//       return callback(new Error('Not allowed by CORS'));
+//     }
+//   },
   credentials: true,
 }));
 
